e4f8b9c82cf5cdd81f654dccfa3245ca2dbd0241
woojoo0 <85680523+woojoo0@users.noreply.github.com>
2021-06-16 15:30:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-// $(document).ready(function(){
+$(document).ready(function(){
 	
-// 	$(".symbol.about").click(function(){
-// 		$(".modal, #modal").show();
-// 	});
+	$(".symbol.about").click(function(){
+		$(".modal, #modal").show();
+	});
 
-// 	$("#modal.modal").click(function(){
-// 		$(".modal").fadeOut(300);
-// 	});
+	$("#modal.modal").click(function(){
+		$(".modal").fadeOut(300);
+	});
 
-// });
-
-// $(function(){
-// 	$('img[usemap]').rwdImageMaps();
-// 	$("#img").width("100%");
-// });
+});
 
 $(document).ready(function(e) {
 	$('img[usemap]').rwdImageMaps();
